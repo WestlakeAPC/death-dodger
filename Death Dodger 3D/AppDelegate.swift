3ce302ee2bbf8f7b7b3096dfbea62397024933e8
Eli Joesph Bradley <elijos03@gmail.com>
2016-09-10 11:57:28 -0500

    Restart work on Death Dodger 3D.
    It’s a three-dimensional macOS app using the same or 3D analogues of the same assets as ‘Death Dodger.’ The old version broke, so I’m receating the target in the main repository.
    
    Signed-off-by: Eli Joesph Bradley <elijos03@gmail.com>

---
+++ Death Dodger 3D/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Death Dodger 3D
+//
+//  Created by Eli Bradley on 9/10/16.
+//  Copyright (c) 2016 Animator Joe. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
---
+++ Death Dodger 3DTests/Death_Dodger_3DTests.swift
@@ -0,0 +1,36 @@
+//
+//  Death_Dodger_3DTests.swift
+//  Death Dodger 3DTests
+//
+//  Created by Eli Bradley on 9/10/16.
+//  Copyright © 2016 Animator Joe. All rights reserved.
+//
+
+import XCTest
+@testable import Death_Dodger_3D
+
+class Death_Dodger_3DTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
