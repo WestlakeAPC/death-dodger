b20d48fffdea93236c7c7562f2378d72c0e09584
Eli Joesph Bradley <elijos03@gmail.com>
2016-09-13 14:37:12 -0500

    Add Xcode Source Editor Extension
    Signed-off-by: Eli Joesph Bradley <elijos03@gmail.com>

---
+++ XcodeEditor/SourceEditorCommand.swift
@@ -0,0 +1,20 @@
+//
+//  SourceEditorCommand.swift
+//  XcodeEditor
+//
+//  Created by Eli Bradley on 9/13/16.
+//  Copyright © 2016 Animator Joe. All rights reserved.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}
